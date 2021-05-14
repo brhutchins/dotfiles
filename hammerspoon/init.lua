0ae33f38b0a5727470c859c536d5b69b0545338d
Barnaby Hutchins <bhutchins@gmail.com>
2021-05-14 11:41:04 +0200

    Removed VimMode

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -15,14 +15,3 @@ local myStackline = {
 }
 
 stackline:init(myStackline)
-
-local VimMode = hs.loadSpoon('VimMode')
-local vim = VimMode:new()
-
-vim
-  :disableForApp('Code')
-  :disableForApp('Emacs')
-  :disableForApp('kitty')
-  :disableForApp('zoom.us')
-  :enterWithSequence('jk')
-  :enableBetaFeature('block_cursor_overlay')
