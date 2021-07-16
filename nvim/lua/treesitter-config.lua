eee372325463495d0a70ac724ec4c368ed345536
Barnaby Hutchins <bhutchins@gmail.com>
2021-07-16 13:57:41 +0200

    Added indentation and incremental selection

--- nvim/lua/treesitter-config.lua
+++ nvim/lua/treesitter-config.lua
@@ -6,4 +6,17 @@ require("nvim-treesitter.configs").setup {
     disable = {},
     additional_vim_regex_highlighting = false,
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_cremental = "grm",
+    },
+  },
+
+  indent = {
+    enable = true
+  },
 }
