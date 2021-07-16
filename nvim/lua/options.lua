afa86fdc47c1f864446b80e781a75b59590b4c75
Barnaby Hutchins <bhutchins@gmail.com>
2021-07-16 11:28:21 +0200

    Added shiftwidth and list

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,9 +1,12 @@
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.number = true
-vim.opt.splitright = true
+vim.opt.expandtab     = true
+vim.opt.tabstop       = 2
+vim.opt.shiftwidth    = 2
+vim.opt.ignorecase    = true
+vim.opt.smartcase     = true
+vim.opt.number        = true
+vim.opt.splitright    = true
 vim.opt.termguicolors = true
+vim.opt.list          = true
+
 vim.o.completeopt = "menuone,noselect"
--- opt.wildmode = {"list", "longest"}
+
