ce659f235a1dffc96d6f018bf15aea9999206b77
Barnaby Hutchins <bhutchins@gmail.com>
2021-07-17 11:35:35 +0200

    LSP fixes and keybindings

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,5 +1,37 @@
 local lspconfig = require("lspconfig")
 
+local on_attach = function(client,bufnr)
+  vim.api.nvim_command [[autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()]]
+
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  --Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+end
+
 -- Snippets
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
@@ -10,6 +42,7 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- Haskell
 lspconfig.hls.setup{
+  on_attach = on_attach,
   cmd = {  "haskell-language-server-wrapper", "--lsp" },
   rootPatterns = { "*.cabal", "stack.yaml", "cabal.project", "package.yaml", "hie.yaml" },
   filetypes = { "haskell", "lhaskell" }
@@ -46,6 +79,7 @@ lspconfig.pyright.setup{}
 
 -- HTML
 lspconfig.html.setup{
+  on_attach = on_attach,
   cmd = { "html-languageserver", "--stdio" },
   filetypes = { "html" },
   init_options = {
@@ -59,4 +93,19 @@ lspconfig.html.setup{
 }
 
 -- Typescript
-lspconfig.tsserver.setup{}
+lspconfig.tsserver.setup{
+  on_attach = on_attach,
+}
+
+-- Lua
+lspconfig.sumneko_lua.setup {
+  on_attach = on_attach,
+  cmd = { "lua-language-server" },
+  settings = {
+    Lua = {
+        diagnostics = {
+          globals = { "vim" },
+      },
+    },
+  },
+}
