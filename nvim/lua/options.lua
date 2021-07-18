960488908a65e9dfee31de50423306c72e08380e
Barnaby Hutchins <bhutchins@gmail.com>
2021-07-18 13:45:16 +0200

    Hasklig as fallback font

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -16,4 +16,8 @@ vim.o.completeopt = "menuone,noselect"
 vim.cmd "au TextYankPost * lua vim.highlight.on_yank {on_visual = false}"
 
 -- GUI font
-  vim.opt.guifont = "Hasklug Nerd Font"
+--- Currently kind of hacky, because we need Hasklug for the devicons, but
+--- Hasklig in the AUR seems to /be/ Hasklug, whereas Hasklig available in
+--- homebrew is not. Coincidentally, my Mac is hdpi whereas my Linux box
+--- currently is not. So we can fix the font sizes this way too.
+vim.opt.guifont = "Hasklug Nerd Font:h13,Hasklig:h10"
