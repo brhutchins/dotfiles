4114b0f20814cee1ea2ec699a0ab09e725a3ef59
Barnaby Hutchins <bhutchins@gmail.com>
2021-07-18 11:38:58 +0200

    Use first completion option on <CR>

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -72,3 +72,6 @@ vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+
+-- Select first completion option on <CR>
+vim.api.nvim_set_keymap("i", "<CR>", "compe#confirm({ 'keys': '<CR>', 'select': v:true })", { expr = true })
