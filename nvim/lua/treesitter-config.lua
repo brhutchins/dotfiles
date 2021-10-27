6005603b513491fe5425e4a885e40b0e16c3fb62
Barnaby Hutchins <bhutchins@gmail.com>
2021-10-27 17:17:12 +0200

    Formatting

--- nvim/lua/treesitter-config.lua
+++ nvim/lua/treesitter-config.lua
@@ -4,7 +4,7 @@ require("nvim-treesitter.configs").setup {
   highlight = {
     enable = true,
     disable = {},
-    additional_vim_regex_highlighting = false,
+    additional_vim_regex_highlighting = false
   },
   incremental_selection = {
     enable = true,
