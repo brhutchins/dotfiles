947ef0c3fa028eb3b88159604a194973e048c531
Barnaby Hutchins <bhutchins@gmail.com>
2021-11-27 12:59:49 +0100

    Now managing xdg default apps

--- home.nix
+++ home.nix
@@ -530,6 +530,28 @@
     '';
   };
 
+  xdg = {
+    enable = true;
+    mime.enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        # Web
+        "x-scheme-handler/http"         = ["firefox.desktop"];
+        "x-scheme-handler/https"        = ["firefox.desktop"];
+        "x-scheme-handler/about"        = ["firefox.desktop"];
+        "x-scheme-handler/unknown"      = ["firefox.desktop"];
+        "text/html"                     = ["firefox.desktop"];
+        "application/x-extension-htm"   = ["firefox.desktop"];
+        "application/x-extension-html"  = ["firefox.desktop"];
+        "application/x-extension-shtml" = ["firefox.desktop"];
+        "application/xhtml+xml"         = ["firefox.desktop"];
+        "application/x-extension-xhtml" = ["firefox.desktop"];
+        "application/x-extension-xht"   = ["firefox.desktop"];
+      };
+    };
+  };
+
   # kitty
   programs.kitty = {
     enable = true;
