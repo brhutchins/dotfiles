01b15f981b1cab5c0d65bdb2c8b9ba1ebf58b731
Barnaby Hutchins <barnaby@Barnabys-iMac-Pro.local>
2021-12-04 11:19:24 +0100

    Working darwin home.nix

--- nix/home-manager/machines/darwin-hubs.nix
+++ nix/home-manager/machines/darwin-hubs.nix
@@ -1,44 +1,12 @@
 { config, pkgs, ... }:
 
-let
-  modulesPath = ./nix/home-manager/modules;
-in
 {
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+     programs.home-manager.enable = true;
 
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "barnaby";
-  home.homeDirectory = "/Users/barnaby";
+     home.username = "barnaby";
+     home.homeDirectory = "/Users/barnaby";
 
-  # Config modules
-  home.file.".config/home-manager".source = ./nix/home-manager/modules;
+     home.file.".config/home-manager".source = ../.dotfiles/nix/home-manager/modules;
 
-  imports = map (x: ./nix/home-manager/modules + x) [
-    "/core/"
-  ];
-
-  local = {
-    core.gui.enable = true;
-  };
-
-  # Packages to install
-  home.packages = with pkgs; [
-  ];
-
-  home.sessionPath = [
-    "$HOME/.emacs.d/bin"
-  ];
-
-  # This value determines the Home Manager release that your
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "22.05";
+     imports = [ ./home-manager/core ];
 }
