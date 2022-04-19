ef9b83d9dd30dbd8e2b8868558e2ca5579c0ccc7
brhutchins <bhutchins@gmail.com>
2022-04-19 13:51:36 +0200

    Update Hammerspoon

--- nix/packages/hammerspoon/default.nix
+++ nix/packages/hammerspoon/default.nix
@@ -6,13 +6,11 @@ stdenv.mkDerivation {
   version = "0.9.91";
 
   src = fetchzip {
-    url = "https://github.com/Hammerspoon/hammerspoon/releases/download/0.9.94/Hammerspoon-0.9.94.zip";
-    sha256 = "sha256-l2oTMujRpZre/NOO7uHJfxd8wFdRHLEVtfSQ0gj5C5U=";
+    url = "https://github.com/Hammerspoon/hammerspoon/releases/download/0.9.97/Hammerspoon-0.9.97.zip";
+    sha256 = "sha256-13cGdXjWeig0hUTdiCmiQdq3wcnbPOjnRRAA7hMn690=";
   };
 
   installPhase = ''
-    # mkdir -p $out/bin
-    # cp ./Contents/Resources/extensions/hs/ipc/bin/hs $out/bin
     mkdir -p $out/Applications/Hammerspoon.app
     mv ./* $out/Applications/Hammerspoon.app
     chmod +x "$out/Applications/Hammerspoon.app/Contents/MacOS/Hammerspoon";
