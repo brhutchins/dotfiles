b0270630b2d456b3a2b8ab3730e90062ea03def3
brhutchins <bhutchins@gmail.com>
2023-05-21 15:02:03 +0200

    fix: zsh syntax highlighting slow paste

--- nix/home-manager/modules/core/default.nix
+++ nix/home-manager/modules/core/default.nix
@@ -156,6 +156,19 @@ in
       # zoxide
       eval "$(zoxide init zsh)"
 
+      ### Fix slowness of pastes with zsh-syntax-highlighting.zsh
+      pasteinit() {
+        OLD_SELF_INSERT=''${''${(s.:.)widgets[self-insert]}[2,3]}
+        zle -N self-insert url-quote-magic
+      }
+
+      pastefinish() {
+        zle -N self-insert $OLD_SELF_INSERT
+      }
+      zstyle :bracketed-paste-magic paste-init pasteinit
+      zstyle :bracketed-paste-magic paste-finish pastefinish
+      ### Fix slowness of pastes
+
       # functions
       gch () {
         if [[ -z $1 ]]; then
