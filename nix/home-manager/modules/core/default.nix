0569dff1ed19ec8a6a73285b10b467b972938801
brhutchins <bhutchins@gmail.com>
2026-03-11 09:08:24 +0100

    tmux catppuccin status bar

--- nix/home-manager/modules/core/default.nix
+++ nix/home-manager/modules/core/default.nix
@@ -309,8 +309,19 @@ in
         {
           plugin = tmuxPlugins.catppuccin;
           extraConfig = ''
-            set -g @catppuccin_flavour 'mocha'
-            set -g @catppuccin_window_tabs_enabled on
+            set -g @catppuccin_flavor "mocha"
+            set -g @catppuccin_window_status_style "rounded"
+            set -g @catppuccin_window_text " #W"
+            set -g @catppuccin_window_current_text " #W"
+            set -g @catppuccin_window_flags "icon"
+
+            set -g status-left-length 100
+            set -g status-right-length 100
+            set -g status-left ""
+            set -g status-right "#{E:@catppuccin_status_directory}"
+            set -ag status-right "#{E:@catppuccin_status_session}"
+            set -ag status-right "#{E:@catppuccin_status_date_time}"
+            set -g @catppuccin_date_time_text " %H:%M  %d %b"
           '';
         }
         tmuxPlugins.resurrect
